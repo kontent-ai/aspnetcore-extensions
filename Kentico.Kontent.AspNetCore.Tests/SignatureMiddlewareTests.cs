4e0bbbdfb2746d5402a1ec3a4655eefa054c82b8
Petr Švihlík <svihlik.petr@gmail.com>
2021-02-26 15:45:57 +0100

    added build procedures and a basic unit test

---
+++ Kentico.Kontent.AspNetCore.Tests/SignatureMiddlewareTests.cs
@@ -0,0 +1,27 @@
+using Kentico.Kontent.AspNetCore.Webhooks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Options;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Kentico.Kontent.AspNetCore.Tests
+{
+    public class SignatureMiddlewareTests
+    {
+        [Fact]
+        public async Task RequestWithoutSignature_ReturnsUnauthorized()
+        {
+            // Arrange
+            var options = Options.Create(new WebhookOptions { });
+            var middleware = new SignatureMiddleware(null, options);
+            var ctx = new DefaultHttpContext();
+
+            // Act
+            await middleware.InvokeAsync(ctx);
+
+            // Assert
+            Assert.Equal(ctx.Response.StatusCode, (int)HttpStatusCode.Unauthorized);
+        }
+    }
+}
