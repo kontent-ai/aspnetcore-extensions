ddcbb1f764d768c83bf431f2d032d4556fa80532
Daniel Pokorny <danielp@kentico.com>
2022-04-25 16:02:59 +0200

    fix: split models to support all webhook types

---
+++ Kentico.Kontent.AspNetCore/Webhooks/Models/Common.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Kentico.Kontent.AspNetCore.Webhooks.Models
+{
+    /// <summary>
+    /// The Message object contains information about the origin of the notification.
+    /// </summary>
+    public class Message
+    {
+        /// <summary>
+        /// Identifier of the webhook.
+        /// </summary>
+        [JsonPropertyName("id"), JsonPropertyOrder(1)]
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Identifier of a Kentico Kontent project.
+        /// </summary>
+        [JsonPropertyName("project_id"), JsonPropertyOrder(2)]
+        public Guid? ProjectId { get; set; }
+
+        /// <summary>
+        /// Type of the object that triggered the webhook (content_item_variant, taxonomy, ...)
+        /// </summary>
+        [JsonPropertyName("type"), JsonPropertyOrder(3)]
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Codename of the operation that triggered the webhook.
+        /// </summary>
+        [JsonPropertyName("operation"), JsonPropertyOrder(4)]
+        public string Operation { get; set; }
+
+        /// <summary>
+        /// Name of the API endpoint, e.g. content_management or delivery_production.
+        /// </summary>
+        [JsonPropertyName("api_name"), JsonPropertyOrder(5)]
+        public string ApiName { get; set; }
+
+        /// <summary>
+        /// Timestamp of the webhook.
+        /// </summary>
+        [JsonPropertyName("created_timestamp"), JsonPropertyOrder(6)]
+        public DateTime CreatedTimestamp { get; set; }
+
+        /// <summary>
+        /// Publicly available URL address of your webhook endpoint.
+        /// </summary>
+        [JsonPropertyName("webhook_url"), JsonPropertyOrder(7)]
+        public string WebhookUrl { get; set; }
+    }
+
+    /// <summary>
+    /// A Taxonomy group object.
+    /// </summary>
+    public class Taxonomy
+    {
+        /// <summary>
+        /// The taxonomy group's internal ID.
+        /// </summary>
+        [JsonPropertyName("id"), JsonPropertyOrder(1)]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The taxonomy group's codename.
+        /// </summary>
+        [JsonPropertyName("codename"), JsonPropertyOrder(2)]
+        public string Codename { get; set; }
+    }
+}
---
+++ Kentico.Kontent.AspNetCore/Webhooks/Models/ManagementWebhookModel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Kentico.Kontent.AspNetCore.Webhooks.Models
+{
+    /// <summary>
+    /// Root object of a Kontent management API triggered webhook.
+    /// </summary>
+    public class ManagementWebhookModel
+    {
+        /// <summary>
+        /// Data relevant to the operation that triggered the webhook.
+        /// </summary>
+        [JsonPropertyName("data"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(1)]
+        public ManagementWebhookData Data { get; set; }
+
+        /// <summary>
+        /// The Message object contains information about the origin of the notification.
+        /// </summary>
+        [JsonPropertyName("message"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(2)]
+        public Message Message { get; set; }
+    }
+
+    /// <summary>
+    /// Data relevant to the operation that triggered the webhook.
+    /// </summary>
+    public class ManagementWebhookData
+    {
+        /// <summary>
+        /// A collection of Item objects for each modified content item.
+        /// </summary>
+        [JsonPropertyName("items"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(1)]
+        public ManagementWebhookItem[] Items { get; set; }
+
+        /// <summary>
+        /// A collection of Taxonomy group objects.
+        /// </summary>
+        [JsonPropertyName("taxonomies"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(2)]
+        public Taxonomy[] Taxonomies { get; set; }
+    }
+
+    /// <summary>
+    /// A Content item object.
+    /// </summary>
+    public class ManagementWebhookItem
+    {
+        /// <summary>
+        /// Reference to an item.
+        /// </summary>
+        [JsonPropertyName("item"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(1)]
+        public Reference Item { get; set; }
+
+        /// <summary>
+        /// Reference to a language.
+        /// </summary>
+        [JsonPropertyName("language"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(2)]
+        public Reference Language { get; set; }
+
+        /// <summary>
+        /// Reference to the workflow step being transitioned from.
+        /// </summary>
+        [JsonPropertyName("transition_from"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(3)]
+        public Reference TransitionFrom { get; set; }
+
+        /// <summary>
+        /// Reference to the workflow step being transitioned to.
+        /// </summary>
+        [JsonPropertyName("transition_to"), JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull), JsonPropertyOrder(4)]
+        public Reference TransitionTo { get; set; }
+    }
+}
