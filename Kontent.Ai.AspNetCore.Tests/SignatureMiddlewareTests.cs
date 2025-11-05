3b1e16771e93099bd8b1b59bc76d02e5d5b3b10d
Martin Ševčík <martins2@kontent.ai>
2025-11-05 16:07:49 +0100

    EN-628 remove legacy webhooks

--- Kontent.Ai.AspNetCore.Tests/SignatureMiddlewareTests.cs
+++ Kontent.Ai.AspNetCore.Tests/SignatureMiddlewareTests.cs
@@ -47,47 +47,5 @@ namespace Kontent.Ai.AspNetCore.Tests
             // Assert
             Assert.Equal(ctx.Response.StatusCode, (int)HttpStatusCode.Unauthorized);
         }
-
-        [Theory]
-        [InlineData("X-KC-Signature", "ezKcSmuYrugdCN73QVWDXREaNY7AkUhFWksUFlp9Tnc=", "PublishDeliveryTriggerWebhookBody.json", "fiJ+MiJxiqmzbnlVzWnR+7Rgas7aDSzsqIApflYiZ4o=", HttpStatusCode.NotFound)]
-        [InlineData("X-KC-Signature", "8UXIHkZ6KuarukFbZDhzyeVAHcKkFnlLabScv9VyNww=", "PublishManagementTriggerWebhookBody.json", "khBZy02UmiUrp2bl1ooPJdILKUmmL2Q7kx318+arMhM=", HttpStatusCode.NotFound)]
-        [InlineData("X-KC-Signature", "8UXIHkZ6KuarukFbZDhzyeVAHcKkFnlLabScv9VyNww=", "PublishDeliveryTriggerWebhookBody.json", "tampered-body-hash", HttpStatusCode.Unauthorized)]
-        [InlineData("X-KC-Signature", "8UXIHkZ6KuarukFbZDhzyeVAHcKkFnlLabScv9VyNww=", "PublishManagementTriggerWebhookBody.json", "tampered-body-hash", HttpStatusCode.Unauthorized)]
-        [InlineData("X-Kontent-ai-Signature", "ezKcSmuYrugdCN73QVWDXREaNY7AkUhFWksUFlp9Tnc=", "PublishDeliveryTriggerWebhookBody.json", "fiJ+MiJxiqmzbnlVzWnR+7Rgas7aDSzsqIApflYiZ4o=", HttpStatusCode.NotFound)]
-        [InlineData("X-Kontent-ai-Signature", "8UXIHkZ6KuarukFbZDhzyeVAHcKkFnlLabScv9VyNww=", "PublishManagementTriggerWebhookBody.json", "khBZy02UmiUrp2bl1ooPJdILKUmmL2Q7kx318+arMhM=", HttpStatusCode.NotFound)]
-        [InlineData("X-Kontent-ai-Signature", "8UXIHkZ6KuarukFbZDhzyeVAHcKkFnlLabScv9VyNww=", "PublishDeliveryTriggerWebhookBody.json", "tampered-body-hash", HttpStatusCode.Unauthorized)]
-        [InlineData("X-Kontent-ai-Signature", "8UXIHkZ6KuarukFbZDhzyeVAHcKkFnlLabScv9VyNww=", "PublishManagementTriggerWebhookBody.json", "tampered-body-hash", HttpStatusCode.Unauthorized)]
-        // https://docs.microsoft.com/en-us/aspnet/core/test/middleware
-        public async Task TriggerRequest_WithValidSignature_CorrectStatusSet(string headerName, string authorizationSecret, string bodyFilename, string signature, HttpStatusCode resultStatus)
-        {
-            using var host = await new HostBuilder()
-                .ConfigureWebHost(webBuilder =>
-                {
-                    webBuilder
-                        .UseTestServer()
-                        .Configure(app =>
-                        {
-                            app.UseWebhookSignatureValidator(context =>
-                                true,
-                                new WebhookOptions
-                                {
-                                    Secret = authorizationSecret
-                                }
-                            );
-                        });
-                })
-                .StartAsync();
-
-            var responsePath = Path.Combine(Environment.CurrentDirectory, "Data", "Legacy", bodyFilename);
-            var server = host.GetTestServer();
-            var context = await server.SendAsync(c =>
-            {
-                c.Request.Method = HttpMethods.Post;
-                c.Request.Headers.Add(headerName, signature);
-                c.Request.Body = new StringContent(File.ReadAllText(responsePath)).ReadAsStream();
-            });
-
-            Assert.Equal(context.Response.StatusCode, (int)resultStatus);
-        }
     }
 }
